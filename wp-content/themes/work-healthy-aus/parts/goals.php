b52a7451ce92760b666ce6516b9b78e0768081af
Eric Stout <ericwstout@gmail.com>
2017-01-10 16:56:45 -0700

    Adds goal repeater and card structure

--- wp-content/themes/work-healthy-aus/parts/goals.php
+++ wp-content/themes/work-healthy-aus/parts/goals.php
@@ -8,17 +8,26 @@
       </h2>
     </div>
   </div>
-  <div class="row">
-    <div class="col-10 col-centered">
-      <div class="sm-block-grid-1 block-grid-2">
-        <div class="col">
-          <div class="service-card">
-            <h3>
-              <?php the_sub_field('goal_headline'); ?> 
-            </h3>
-          </div>
+  <?php if( have_rows('goals') ): ?>
+    <div class="row">
+      <div class="col-10 col-centered">
+        <div class="sm-block-grid-1 block-grid-2">
+          <?php while( have_rows('goals') ): the_row(); ?>
+            <div class="col">
+              <a href="<?php the_sub_field('goal_page');?>" class="service-card">
+                <h3>
+                  <?php the_sub_field('goal_headline'); ?>
+                </h3>
+                <p>
+                  <em>
+                    <?php the_sub_field('goal_description'); ?>
+                  </em>
+                </p>
+              </a>
+            </div>
+          <?php endwhile; ?>
         </div>
       </div>
     </div>
-  </div>
+  <?php endif; ?>
 </section>
