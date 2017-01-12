1ec752005476c256d7ed51062830b9ffe6187033
Eric Stout <ericwstout@gmail.com>
2017-01-12 16:05:04 -0700

    Adds home video, temporary

--- wp-content/themes/work-healthy-aus/parts/home-hero.php
+++ wp-content/themes/work-healthy-aus/parts/home-hero.php
@@ -3,7 +3,12 @@
 <div class="container">
   <div class="row row--full-width">
     <div class="col-12">
-      <img src="<?php bloginfo('template_url');?>/assets/img/video-cover.jpg" alt="Work Healthy Australia">
+      <div class="video-container">
+        <video width="100%" muted autoplay poster="<?php bloginfo('template_url');?>/assets/img/video-cover.jpg">
+          Sorry, your browser doesn't support embedded videos.
+          <source src="<?php bloginfo('template_url');?>/assets/video/intro-video.mp4" type="video/mp4">
+        </video>
+      </div>
     </div>
   </div>
 </div>
