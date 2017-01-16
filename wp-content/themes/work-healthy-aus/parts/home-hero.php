33feaa956baa4e6800bf85aa869f95a3258754f5
Eric Stout <ericwstout@gmail.com>
2017-01-16 15:44:26 -0700

    hides video on small screens

--- wp-content/themes/work-healthy-aus/parts/home-hero.php
+++ wp-content/themes/work-healthy-aus/parts/home-hero.php
@@ -3,12 +3,13 @@
 <div class="container">
   <div class="row row--full-width">
     <div class="col-12">
-      <div class="video-container">
+      <div class="video-container sm-hide">
         <video width="100%" muted autoplay poster="<?php bloginfo('template_url');?>/assets/img/video-cover.jpg">
           Sorry, your browser doesn't support embedded videos.
           <source src="<?php bloginfo('template_url');?>/assets/video/intro-video.mp4" type="video/mp4">
         </video>
       </div>
+      <img src="<?php bloginfo('template_url');?>/assets/img/video-cover.jpg" alt="" role="presentation" class="sm-only">
     </div>
   </div>
 </div>
