a048d0067f62daed412f23ac7fcb45c0a907aae9
Eric Stout <ericwstout@gmail.com>
2017-01-17 09:37:02 -0700

    Hero testing

--- parts/standard-hero.php
+++ parts/standard-hero.php
@@ -8,11 +8,11 @@ $poop = true;
     <div class="col-12">
       <h1>
         <?php
-        if( is_page_template('templates/goal.php') || basename(get_page_template()) === 'page.php' ):
+        if( $poop === true ):
+          echo 'Page Not Found';
+        elseif( is_page_template('templates/goal.php') || basename(get_page_template()) === 'page.php' ):
           the_title();
           echo 'this is goal or page template';
-        elseif( $poop === true ):
-          echo 'Page Not Found';
         else:
           the_field('page_headline');
           echo 'else statement fired';
