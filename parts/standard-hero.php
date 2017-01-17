ca52f25b7cf004d53cf449919676925c4023dc3c
Eric Stout <ericwstout@gmail.com>
2017-01-17 09:44:58 -0700

    Updates standad hero text output

--- parts/standard-hero.php
+++ parts/standard-hero.php
@@ -9,10 +9,8 @@
           echo 'Page Not Found';
         elseif( is_page_template('templates/goal.php') || basename(get_page_template()) === 'page.php' ):
           the_title();
-          echo 'this is goal or page template';
         else:
           the_field('page_headline');
-          echo 'else statement fired';
         endif;?>
       </h1>
     </div>
