2f71ada3b429075a6dd6ab3be0678966006c65e0
Eric Stout <ericwstout@gmail.com>
2017-01-16 17:49:01 -0700

    Restructures repo to exclude wordpress

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+  /**
+   * The default blog / index template.
+   */
+  get_header();
+?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+  <section class="main-content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h1 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <?php
+          if ( has_post_thumbnail() ) {
+            the_post_thumbnail();
+          }
+          get_template_part( 'parts/meta' );
+          the_content();
+        ?>
+      </article>
+      <?php
+    endwhile;
+      the_posts_pagination( array('mid_size' => 2) );
+    else :
+      ?>
+      <h2>Sorry, no posts have been found</h2>
+    <?php endif; ?>
+  </section>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<?php
+  get_footer();
