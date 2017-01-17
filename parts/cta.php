c0465f29f44a4dadb009b11490499e9a6462c6eb
Eric Stout <ericwstout@gmail.com>
2017-01-17 14:32:38 -0700

    Updates cta to use page id's for fields

--- parts/cta.php
+++ parts/cta.php
@@ -7,14 +7,16 @@
     </div>
     <div class="md-col-6 col-3 sm-text-center md-text-center">
       <?php
-        if( get_field('cta_button_destination') === 'internal' ):
-          $cta_link = get_field('cta_internal');
+      $page_id = $post->ID;
+
+        if( get_field('cta_button_destination', $page_id) === 'internal' ):
+          $cta_link = get_field('cta_internal', $page_id);
         else:
-          $cta_link = get_field('cta_external');
+          $cta_link = get_field('cta_external', $page_id);
         endif;
        ?>
        <a href="<?php echo $cta_link;?>" class="button button--gold">
-         <?php the_field('cta_button_text'); ?>
+         <?php the_field('cta_button_text', $page_id); ?>
        </a>
     </div>
   </div>
