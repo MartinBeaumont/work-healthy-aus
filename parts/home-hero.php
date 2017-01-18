779bfa48a5b7a3d11e4adc1b47e3fbefc46470f6
Eric Stout <ericwstout@gmail.com>
2017-01-18 10:39:25 -0700

    Adds new video with new filename

--- parts/home-hero.php
+++ parts/home-hero.php
@@ -6,7 +6,7 @@
       <div class="video-container sm-hide">
         <video width="100%" muted autoplay poster="<?php bloginfo('template_url');?>/assets/img/video-cover.jpg">
           Sorry, your browser doesn't support embedded videos.
-          <source src="<?php bloginfo('template_url');?>/assets/video/intro-video.mp4" type="video/mp4">
+          <source src="<?php bloginfo('template_url');?>/assets/video/opening-video.mp4" type="video/mp4">
         </video>
       </div>
       <img src="<?php bloginfo('template_url');?>/assets/img/video-cover.jpg" alt="" role="presentation" class="sm-only">
