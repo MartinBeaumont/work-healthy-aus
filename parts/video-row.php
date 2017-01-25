e53cc3e9f6158342c87d96b2ef3bb4b91be2935d
Eric Stout <ericwstout@gmail.com>
2017-01-25 09:29:45 -0700

    Adds video embed acf value

--- parts/video-row.php
+++ parts/video-row.php
@@ -3,7 +3,9 @@
 <section class="container video-row">
   <div class="row">
     <div class="col-10 col-centered">
-
+			<div class="flex-video">
+				<?php the_field('video_embed'); ?>
+			</div>
     </div>
   </div>
 </section>
