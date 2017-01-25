b7cf8700bf3333dd1bc3dcbf60830d47dd6286dc
Eric Stout <ericwstout@gmail.com>
2017-01-25 10:50:19 -0700

    Updates style for login page

--- inc/login-styles.php
+++ inc/login-styles.php
@@ -4,12 +4,26 @@
 	// add login logo
 	function custom_loginlogo() {
 	echo '<style type="text/css">
+	body{
+		background: #E6E7E8;
+	}
+
+	.wp-core-ui .button-primary {
+    background: #E75300;
+    border-color: #E75300 #E75300 #E75300;
+    -webkit-box-shadow: 0 1px 0 #006799;
+    box-shadow: 0 1px 0 #006799;
+    color: #fff;
+    text-decoration: none;
+    text-shadow: 0 -1px 1px #006799, 1px 0 1px #006799, 0 1px 1px #006799, -1px 0 1px #006799;
+}
+
 	h1 a {
 		height: 100% !important;
 		width:100% !important;
 		background-image: url('.get_bloginfo('template_directory').'/assets/img/logo.svg) !important;
 		background-postion-x: center !important;
-		background-size:100% !important;
+		background-size: contain !important;
 		margin-bottom:10px !important; }
 
 	h1 {
