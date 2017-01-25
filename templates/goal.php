7faebc670c8852b8aa36e6134d6a50fabd80608a
Eric Stout <ericwstout@gmail.com>
2017-01-25 11:50:00 -0700

    Query debugging

--- templates/goal.php
+++ templates/goal.php
@@ -13,7 +13,7 @@ echo '</pre>';
 // WP_Query arguments
 $args = array(
 	//'p'                 => $post_ids,
-  //'post_type'         => array( 'services' ),
+  'post_type'         => array( 'services' ),
 	'post__in'					=> array($post_ids)
 );
 
