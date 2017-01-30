0858bb108f67e60bf99e5cf95ae43c1c611b58f2
Eric Stout <ericwstout@gmail.com>
2017-01-30 15:22:27 -0700

    Updates 404 copy

--- 404.php
+++ 404.php
@@ -16,11 +16,16 @@
     </div>
     <div class="col-10 col-centered">
       <h2>
-        This page does not exist
+        Sorry, we couldn't find that page
       </h2>
-      <a href="<?php echo get_home_url();?>/">
-        Go back home
-      </a>
+
+      <p>
+        While we were busy treating injuries in the workplace, you discovered
+        one on our website! Head back to the <a href="<?php echo get_home_url();?>/">home page</a> to
+        learn more about Work Healthy Australia, or <a href="<?php echo get_home_url();?>/contact/">contact us</a>
+        if you can't find the information you're looking for.
+      </p>
+
     </div>
   </div>
 </section>
